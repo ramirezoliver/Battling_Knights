d79639d79525a23135d63943cab880b194b524b0
Oliver Ramirez (AV-PH) <61497306+ramirezoliver@users.noreply.github.com>
2022-02-19 18:45:53 +0800

    update flake8 lint

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from typing import List, Dict, Any
 import json
 from dataclasses import dataclass, astuple
 
+
 @dataclass(unsafe_hash=True)
 class Point():
     y: int
     x: int
-    
+
     def move(self, dir_code: str):
         if dir_code == 'N':
             self.y += -1
@@ -26,22 +27,26 @@ class KnightState():
     attack: int = 1
     defense: int = 1
 
+
 @dataclass
 class Knight():
     code: str
     name: str
     state: KnightState
 
+
 @dataclass
 class ItemState():
     position: Point
     equipped: bool = False
+
 
 @dataclass
 class ItemStats():
     attack: int
     defense: int
     priority: int
+
 
 @dataclass
 class Item():
@@ -51,59 +56,65 @@ class Item():
 
 
 class BattleKnights():
-    DIRECTIONS: Dict = {"N": Point(-1,0), "S": Point(1,0) , "E": Point(0,1) ,"W": Point(0,-1)}
-    def __init__(self, size: Point, list_knights: List[Knight], list_items: List[Item]):
-        status_knights: Dict = {KN.name: KN.state for KN in list_knights}
-        status_items: Dict = {IT.name: IT.state for IT in list_items}
+    def __init__(self, size: Point, knights: List[Knight], items: List[Item]):
+
+        status_knights = {KN.name: KN.state for KN in knights}
+        status_items = {IT.name: IT.state for IT in knights}
 
         self.status: Dict = {**status_knights, **status_items}
-        self.items_stats: Dict = {IT.name: IT.stats for IT in list_items} #item A/D table
-        self.knight_codes: Dict = {KN.code: KN.name for KN in list_knights}
-        self.board_knights: Dict = {KN.state.position: KN.name for KN in list_knights} #positions of LIVE Knights
-        self.board_free_items: Dict = {IT.state.position: IT.name for IT in list_items} #positions of free Items
         self.size: Point = size
 
+        # positions of LIVE Knights
+        self.board_KN = {KN.state.position: KN.name for KN in knights}
+        self.KN_code_map = {KN.code: KN.name for KN in knights}
+
+        # item A/D table
+        self.items_stats = {IT.name: IT.stats for IT in items}
+
+        # positions of free Items
+        self.board_IT = {IT.state.position: IT.name for IT in items}
+
     def progress_game(self, line: str, lineno: int) -> Dict:
+        DIRECTIONS: set = {"N", "S", "W", "E"}
         step = line.split(':')
         if len(step) != 2:
             raise Exception(f'Invalid step format in line {lineno}')
-        elif step[0] not in self.knight_codes:
+        elif step[0] not in self.KN_code_map:
             raise Exception(f'Invalid knight code in line {lineno}')
-        elif step[1] not in self.DIRECTIONS:
+        elif step[1] not in DIRECTIONS:
             raise Exception(f'Invalid direction in line {lineno}')
         self.game_step(step[0], step[1])
-        return self.status
-
-    def game_step(self, KN_code: str, dir_code: str) -> Dict:
-        name = self.knight_codes[KN_code]
-        self.status[name].position.move(dir_code)
         return self.status
 
     def status_json(self):
         status = {k: astuple(v) for k, v in self.status.items()}
         return json.dumps(status)
 
+    def game_step(self, KN_code: str, dir_code: str) -> Dict:
+        name = self.KN_code_map[KN_code]
+        self.status[name].position.move(dir_code)
+        return self.status
+
 
 if __name__ == '__main__':
     game = BattleKnights(
-        Point(8,8),
+        Point(8, 8),
         [
-            Knight('R','red',KnightState(Point(1,2))),
-            Knight('B','blue',KnightState(Point(1,5)))
+            Knight('R', 'red', KnightState(Point(1, 2))),
+            Knight('B', 'blue', KnightState(Point(1, 5)))
         ],
         [
-            Item('magic_staff',ItemStats(1,0,0), ItemState(Point(0,0)))
+            Item('magic_staff', ItemStats(1, 0, 0), ItemState(Point(0, 0)))
         ]
     )
 
-    with open ("moves.txt") as f:
-        for lineno, line in enumerate(f, start= 1):
+    with open("moves.txt") as f:
+        for lineno, line in enumerate(f, start=1):
             line = line.strip()
             if line == "GAME-END":
                 break
             elif line == "GAME-START":
-                continue                    
-            game.progress_game(line,lineno)
-            
+                continue
+            game.progress_game(line, lineno)
+
             print(game.status_json())
-            
--- tests/ut/test_main.py
+++ tests/ut/test_main.py
@@ -1,23 +1,25 @@
 from app.main import BattleKnights, Knight, KnightState, Point
 
+
 def test_movement():
     test_game = BattleKnights(
-        (2,2),
-        [Knight('R', 'red', KnightState(Point(0,0)))],
+        (2, 2),
+        [Knight('R', 'red', KnightState(Point(0, 0)))],
         []
     )
 
-    assert test_game.status['red'].position == Point(0,0)
+    assert test_game.status['red'].position == Point(0, 0)
 
-    #perform a loop
+    # perform a loop
     status = test_game.game_step('R', 'S')
-    assert status['red'].position == Point(1,0)
+    assert status['red'].position == Point(1, 0)
 
     status = test_game.game_step('R', 'E')
-    assert status['red'].position == Point(1,1)
+    assert status['red'].position == Point(1, 1)
 
     status = test_game.game_step('R', 'N')
-    assert status['red'].position == Point(0,1)
+    assert status['red'].position == Point(0, 1)
 
     status = test_game.game_step('R', 'W')
-    assert status['red'].position == Point(0,0)
+    assert status['red'].position == Point(0, 0)
+
